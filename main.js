2d22982a90485e094de4a99c78ccfd770d3d0638
Andrey <andreypo2016@gmail.com>
2019-05-27 14:58:32 +0300

    first task

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
-let one = false,
-	two = true;
+'use strict';
 
-console.log(one && two);
+let money = prompt('Ваш бюджет на месяц?'),
+	time = prompt('Введите дату в формате YYYY-MM-DD')
+
+
+let answerOne = prompt('Введите обязательную статью расходов в этом месяце'),
+	answerTwo = prompt('Во сколько обойдется?');
+
+let appData = {
+	money,
+	timeData: time,
+	expenses: {
+		[answerOne]: answerTwo
+	},
+	optionalExpenses: {
+
+	},
+	incom: [
+
+	],
+	sacing: false
+};
+
+console.log(appData);
+
+alert('бюджет на 1 день -' + money/30 + 'rub');
