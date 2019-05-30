13a4704916688c787e465cbe19c088ca26fde002
Andrey <andreypo2016@gmail.com>
2019-05-31 01:08:31 +0300

    5 task. Расчет дневного бюджета 1

--- main.js
+++ main.js
@@ -1,97 +1,22 @@
-let money, time;
+let start = document.getElementById('start'),
+	budget = document.getElementsByClass('budget-value'),
+	daybudget = document.getElementsByClass('daybudget-value'),
+	level = document.getElementsByClass('level-value'),
+	expenses = document.getElementsByClass('expenses-value'),
+	optionalexpenses = document.getElementsByClass('optionalexpenses-value'),
+	income = document.getElementsByClass('income-value'),
+	monthsavings = document.getElementsByClass('monthsavings-value'),
+	yearsavings = document.getElementsByClass('yearsavings-value'),
+	inp = document.getElementsByClass('expenses-item'),
+	buttonOne = document.getElementByTagName('button'),
+	buttonTwo = document.getElementByTagName('button'),
+	optionalexpensesItem = document.querySelectorAll('.data .optionalexpenses-item'),
+	chooseIncome = document.querySelector('.choose-income'),
+	percent = document.querySelector('.choose-percent'),
+	year = document.querySelector('.year-value'),
+	month = document.querySelector('.month-value'),
+	day = document.querySelector('.day-value'),
+	sum = document.querySelector('.choose-sum'),
+	savings = document.querySelector('.savings')
 
-function start(argument) {
-	money = +prompt('Ваш бюджет на месяц?');
-	time = prompt('Введите дату в формате YYYY-MM-DD');
-
-//проверка на пустую строку, не число, отмену
-	while(isNaN(money) || money == '' || money == null) {
-		money = +prompt('Ваш бюджет на месяц?', '');
-	}
-}
-start();
-
-let appData = {
-	money,
-	timeData: time,
-	expenses: {},
-	optionalExpenses: {},
-	incom: [],
-	saving: true,
-	shooseExpenses: function() {
-		for (let i = 0; i < 2; i++) {
-		let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
-		 	b = prompt('Во сколько обойдется?', '');
-		if (typeof(a) === 'string' && typeof(a) != null && typeof(b) === 'string' && typeof(b) != null
-			&& a != '' && b != '' && a.length < 30) {
-			console.log('good');
-			appData.expenses[a] = b;
-		} else {
-			console.log('bed result');
-			i--;
-			}
-		}
-	},
-	//Дневной бюджет
-	detectDayBudget: function() {
-		appData.moneyForDay = +(appData.money/30).toFixed();
-		alert(appData.moneyForDay);
-	},
-	// уровень достатка
-	detectLevel: function() {
-		if (appData.moneyForDay < 100) {
-			console.log('min');
-		} else if (appData.moneyForDay > 100 && appData.moneyForDay < 200) {
-			console.log('mid');
-		} else if (appData.moneyForDay > 200) {
-			console.log('max');
-		} else {
-			console.log('arror');
-		}
-	},
-	//сбережения и %
-	scheckSave: function() {
-		if (appData.saving == true) {
-		let save = prompt('how', '');
-			procent = prompt('%', '');
-
-		appData.monthIn = save/100/12*procent;
-		alert('доход ' + appData.monthIn);
-		}
-	},
-	//определения необязательных расходов
-	chooseOptExpenses: function() {
-		for (let i = 1; i < 4; i++ ) {
-		let a = prompt('Статья необязательных расходов?', '');
-		appData.optionalExpenses[i] = a;
-		}
-	},
-	//дополнительные доходы
-	chooseInCome: function() {
-		let items = prompt('что принесет доп доход (введите через запятую)', '');
-//(!!items && items.match(/[0-9]/)) || !items - Ромына проверка
-		while (!isNaN(items) || items == '' || items == null) {
-			items = prompt('что принесет доп доход (введите через запятую)', '');
-		}
-		appData.incom = items.split(', ');
-		appData.incom.push(prompt('mb you forgot somethin?', ''));
-		appData.incom.sort();
-
-		// alert('Способы доп. заработка: \n -' + appData.incom.join('\n- '))
-		let huy = 0;
-		alert(
-        'Способы доп. заработка: ' +
-        appData.incom
-            .map(function (item) {
-            	huy++;
-                return '\n' + huy + '. ' + item;
-            })
-            .join('\n')
-    	)
-	}
-};
-
-console.log('Наша программа включает в себя данные:');
-for (let key in appData) {
-	console.log(key);
-}
+console.log(start);
---
+++ task 5. Расчет дневного бюджета/main.js
@@ -0,0 +1,22 @@
+let start = document.getElementById('start'),
+	budget = document.getElementsByClassName('budget-value'),
+	daybudget = document.getElementsByClassName('daybudget-value'),
+	level = document.getElementsByClassName('level-value'),
+	expenses = document.getElementsByClassName('expenses-value'),
+	optionalexpenses = document.getElementsByClassName('optionalexpenses-value'),
+	income = document.getElementsByClassName('income-value'),
+	monthsavings = document.getElementsByClassName('monthsavings-value'),
+	yearsavings = document.getElementsByClassName('yearsavings-value'),
+	inp = document.getElementsByClassName('expenses-item'),
+	buttonOne = document.getElementsByTagName('button'),
+	buttonTwo = document.getElementsByTagName('button'),
+	optionalexpensesItem = document.querySelectorAll('.data .optionalexpenses-item'),
+	chooseIncome = document.querySelector('.choose-income'),
+	percent = document.querySelector('.choose-percent'),
+	year = document.querySelector('.year-value'),
+	month = document.querySelector('.month-value'),
+	day = document.querySelector('.day-value'),
+	sum = document.querySelector('.choose-sum'),
+	savings = document.querySelector('.savings')
+
+console.log(optionalexpensesItem);
